983c6c2c8f708a1e8ac8e2b6dbf6c39a7d9f3dd4
유선혁 <tjsgur2399@icloud.com>
2025-06-04 22:02:05 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+export default function App() {
+
+    return (
+        <>
+            <p>
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    )
+}
+
+
---
+++ src/api/axios.ts
@@ -0,0 +1,4 @@
+// src/api/axios.ts
+
+// 1. axios 라이브러리 불러옴
+import axios from 'axios';
